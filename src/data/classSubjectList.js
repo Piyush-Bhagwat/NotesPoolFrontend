0b81afeb7ae34caeac4f3b53ae27f9324c189437
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2023-05-06 17:41:42 +0530

    Added all sem subjects

--- src/data/classSubjectList.js
+++ src/data/classSubjectList.js
@@ -23,6 +23,17 @@ const subjectList = [
         ],
     },
     {
+        class: "BCA_3",
+        subjects: [
+            "visual_basic",
+            "dbms",
+            "data-structure",
+            "operation_research_1",
+            "web_technologies_1",
+            "digital_electronic_1",
+        ],
+    },
+    {
         class: "BCA_4",
         subjects: [
             "operation_research_2",
@@ -30,9 +41,35 @@ const subjectList = [
             "toc",
             "sql",
             "web_technologies_2",
-            "software_engneering",
+            "software_engineering",
         ],
     },
+    {
+        class: "BCA_5",
+        subjects: [
+            "computer_graphic_1",
+            "compiler_construction",
+            "vb.net",
+            "software_engineering_2",
+            "php_1",
+            "data_communication_1",
+        ],
+    },
+    
+    {
+        class: "BCA_6",
+        subjects: [
+            "computer_graphic_2",
+            "java",
+            "asp.net",
+            "software_testing",
+            "php_2",
+            "data_communication_2",
+        ],
+    },
+    
+    
+    
 ];
 
 export default subjectList;
