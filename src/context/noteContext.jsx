6b503d3a2375d281535e18cea466a61f3f85648c
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2023-05-08 00:41:06 +0530

    changed port to global

--- src/context/noteContext.jsx
+++ src/context/noteContext.jsx
@@ -15,7 +15,7 @@ export default function NoteContextProvider(props) {
     const globalHost = "https://notepool-backend.onrender.com";
     const localHost = "http://localhost:4040";
 
-    const bPORT = localHost;
+    const bPORT = globalHost;
 
     const toastSettings = {
         position: "bottom-left",
